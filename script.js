6d9b1b430680b7f9ac4097be5c8b33d1d62c3ae1
Odysseus Abraham Kirikopoulos <odysseusk@proton.me>
2024-06-23 13:42:45 +0000

    Added windows to all apps

--- script.js
+++ script.js
@@ -1,68 +1,86 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Show the welcome screen when the page loads
-  document.getElementById("welcomeScreen").style.display = "flex";
+    document.getElementById("welcomeScreen").style.display = "flex";
+    function updateTime() {
+        const currentTime = new Date().toLocaleTimeString();
+        document.getElementById("time").innerText = currentTime;
+    }
 
-  // Function to update the time
-  function updateTime() {
-      const currentTime = new Date().toLocaleTimeString();
-      document.getElementById("time").innerText = currentTime;
-  }
+    setInterval(updateTime, 1000);
+    updateTime(); 
+  
+    function makeElementDraggable(elmnt) {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+        if (elmnt.querySelector(".windowHeader")) {
+            elmnt.querySelector(".windowHeader").onmousedown = dragMouseDown;
+        } else {
+            elmnt.onmousedown = dragMouseDown;
+        }
+  
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+  
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+  
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+  
+    const windows = document.querySelectorAll(".window");
+    windows.forEach(window => {
+        makeElementDraggable(window);
+    });
+  
+    document.getElementById("welcomeClose").onclick = function() {
+        document.getElementById("welcomeScreen").style.display = "none";
+    }
 
-  // Update the time every second
-  setInterval(updateTime, 1000);
-  updateTime();  // Initial call to display the time immediately on load
+    document.getElementById("educationClose").onclick = function() {
+        document.getElementById("educationScreen").style.display = "none";
+    }
 
-  // Function to make the element draggable
-  function makeElementDraggable(elmnt) {
-      let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    document.getElementById("projectsClose").onclick = function() {
+        document.getElementById("projectsScreen").style.display = "none";
+    }
 
-      if (elmnt.querySelector(".windowHeader")) {
-          elmnt.querySelector(".windowHeader").onmousedown = dragMouseDown;
-      } else {
-          elmnt.onmousedown = dragMouseDown;
-      }
+    document.getElementById("awardsClose").onclick = function() {
+        document.getElementById("awardsScreen").style.display = "none";
+    }
 
-      function dragMouseDown(e) {
-          e = e || window.event;
-          e.preventDefault();
-          pos3 = e.clientX;
-          pos4 = e.clientY;
-          document.onmouseup = closeDragElement;
-          document.onmousemove = elementDrag;
-      }
+    document.getElementById("contactClose").onclick = function() {
+        document.getElementById("contactScreen").style.display = "none";
+    }
+  
+    document.getElementById("education").onclick = function() {
+        document.getElementById("educationScreen").style.display = "flex";
+    }
 
-      function elementDrag(e) {
-          e = e || window.event;
-          e.preventDefault();
-          pos1 = pos3 - e.clientX;
-          pos2 = pos4 - e.clientY;
-          pos3 = e.clientX;
-          pos4 = e.clientY;
-          elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-          elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
+    document.getElementById("projects").onclick = function() {
+        document.getElementById("projectsScreen").style.display = "flex";
+    }
 
-      function closeDragElement() {
-          document.onmouseup = null;
-          document.onmousemove = null;
-      }
-  }
+    document.getElementById("awards").onclick = function() {
+        document.getElementById("awardsScreen").style.display = "flex";
+    }
 
-  // Make the windows draggable
-  makeElementDraggable(document.getElementById("welcomeScreen"));
-  makeElementDraggable(document.getElementById("educationScreen"));
-
-  // Handle close buttons
-  document.getElementById("welcomeClose").onclick = function() {
-      document.getElementById("welcomeScreen").style.display = "none";
-  }
-
-  document.getElementById("educationClose").onclick = function() {
-      document.getElementById("educationScreen").style.display = "none";
-  }
-
-  // Handle education icon click to show education screen
-  document.getElementById("education").onclick = function() {
-      document.getElementById("educationScreen").style.display = "flex";
-  }
-});
+    document.getElementById("contact").onclick = function() {
+        document.getElementById("contactScreen").style.display = "flex";
+    }
+});  
